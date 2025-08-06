f4269a66c1e361cc492cbe8a6d54453bf57c70e6
Saboor Bakshi <saboor.bakshi@icloud.com>
2025-08-06 19:10:39 -0400

    initial commit

--- Test/ContentView.swift
+++ Test/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        HStack(content: {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        })
+        .foregroundColor(.orange)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 16).fill(.white)
+                RoundedRectangle(cornerRadius: 16).stroke(lineWidth: 2)
+                Text("😁")
+                    .font(.largeTitle)
+            }
+            else {
+                RoundedRectangle(cornerRadius: 16)
+            }
+        })
     }
 }
 
